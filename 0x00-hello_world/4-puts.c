0fbf5cff3e671858af964ca0a7bf6a6ec87daa59
Nunoped <pedroozor2016@hotmail.com>
2022-09-08 16:23:47 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+*main - print the string in the put function
+*
+* description: using the main function
+* this program prints " Its not easy"
+
+* return : 0
+*/
+int main(void)
+{
+     puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
