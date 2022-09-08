0c4ef7edd750f13171bdfaf25f90d6ea6a4aae76
Nunoped <pedroozor2016@hotmail.com>
2022-09-08 16:56:38 -0700

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,8 @@
-#include <stdio.h>
+#include <stdio.h
+
 /** 
 * main - print the string in the put function
-* 
+*                                     
 * Description: using the main function
 * this progrsm prints "Programming is like building a multilingual puzzle
 * Return: 0
